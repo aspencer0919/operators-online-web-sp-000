2918e1a000093f9a091cb1a1b3f4937681835bf1
Adam Spencer <aspencer0919@gmail.com>
2020-03-20 02:39:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,6 +8,7 @@ end
 
 
 def not_safe?(speed)
-	if speed > 60 || speed < 40 ? return true : return false
+	if speed > 60 ? return false : return true
+	elsif speed < 40 ? return false : return true
 	end
 end
