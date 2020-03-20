203e40c0a18760e3e6148927b106e18b203ba6bc
Adam Spencer <aspencer0919@gmail.com>
2020-03-20 02:57:24 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,17 +8,6 @@ end
 
 
 def not_safe?(speed)
-	if speed > 60
-		test_1 == 1
-	else
-		test_1 == 0
-	end
-	if speed < 40
-		test_2 == 1
-	else
-		test_2 == 0
-	end
-	test_3 == test_1 + test_2
-	if test_3 == 2 ? false : true
+	if speed.between?(40,60) ? false : true
 	end
 end
